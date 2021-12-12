ee3e43cbda8ea67d5108606d44a12bedaaf7daef
vimtum@tutanota.com <vimtum@tutanota.com>
2021-12-12 20:45:11 +0300

    First task in openCV (hw.py) / cleanup

---
+++ OpenCV_learn/hw.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import sys
+import numpy as np
+
+def main():
+    height = 620
+    width = 440
+    image = np.zeros((height, width, 3), np.uint8)
+    font = cv.FONT_HERSHEY_COMPLEX
+    window_name = "hw_window"
+    
+    cv.putText(image, "Hello world!", (0, height //2), font, 1, (150, 0, 150), 2)
+    cv.imshow(window_name, image)
+    cv.waitKey(0)
+    cv.destroyWindow(window_name)
+    return
+
+if __name__ == "__main__":
+    main()
